144b6e9ad4c104fc85b6f01fea3981b3faa30731
Juan Basgall <juanangelbasgall@hotmail.com>
2026-02-18 00:58:37 -0300

    security: Added rate limit, balcklist, and api key verification due to force attacks on server

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,16 +1,19 @@
 export const logger = (req, res, next) => {
     const start = Date.now();
-    console.log(`IN  [${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+    // Obtenemos la IP. Si req.ip no basta, a veces se usa req.headers['x-forwarded-for']
+    const ip = req.ip || req.connection.remoteAddress || '0.0.0.0';
+    
+    console.log(`IN  [${new Date().toISOString()}] ${ip} | ${req.method} ${req.originalUrl}`);
     
     res.on('finish', () => {
         const duration = Date.now() - start;
 
-        let color = '\x1b[32m'; // verde
-        if (res.statusCode >= 400) color = '\x1b[33m'; // amarillo
-        if (res.statusCode >= 500) color = '\x1b[31m'; // rojo
+        let color = '\x1b[32m'; // verde (2xx)
+        if (res.statusCode >= 400) color = '\x1b[33m'; // amarillo (4xx)
+        if (res.statusCode >= 500) color = '\x1b[31m'; // rojo (5xx)
 
         console.log(
-            `${color}OUT [${new Date().toISOString()}] ${req.method} ${req.originalUrl} → ${res.statusCode} (${duration}ms)`
+            `${color}OUT [${new Date().toISOString()}] ${ip} | ${req.method} ${req.originalUrl} → ${res.statusCode} (${duration}ms)\x1b[0m`
         );
     });
     
